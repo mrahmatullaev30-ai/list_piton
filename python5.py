474bdc5f4a792e72a9baf9a760557c27696bb9e3
mrahmatullaev30-ai <mrahmatullaev30@gmail.com>
2026-01-06 19:06:06 +0500

    slp

---
+++ python5.py
@@ -0,0 +1,5 @@
+list2 = [[1,2,3], [4,5,6], [10,11,12], [7,8,9]]
+
+max_list = max(list2, key = sum)
+
+print(max_list)
---
+++ pythonlist6.py
@@ -0,0 +1,5 @@
+lst = [1, 2, 3, 4]
+s = "emp"
+
+result = [s + str(x) for x in lst]
+print(result)
